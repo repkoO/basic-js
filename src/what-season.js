e5d921bfe84e18b19936b3896305f8d09a65bf25
Kirill Medvedev <repkomail@gmail.com>
2023-12-25 23:10:20 +0200

    feat: andd encode-line solve

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,20 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(date) {
   let month = date.getMonth(date);
-  if (!date) { return 'Unable to determine the time of year!'
-  } else  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length)  { 
-    throw new Error("Invalid date!") } 
+  if (!date) return 'Unable to determine the time of year!'
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length)  {
+    throw new Error("Invalid date!") }
     if (month >= 2 && month <= 4) {
     return 'spring'
   } else if (month >= 5 && month <= 7) {
